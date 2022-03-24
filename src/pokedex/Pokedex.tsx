8cfd08ba225d508504209dbd5cce1eb320f9a160
Pedro Mendes <pedrohh0012@icloud.com>
2022-03-24 19:04:05 -0300

    GET na API atraves do axios

--- src/pokedex/Pokedex.tsx
+++ src/pokedex/Pokedex.tsx
@@ -1,24 +1,46 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 interface PokedexProps {
     
 }
 
-const pokemonsArray: string[] = ["Charmander", "Lucario", "Lugia", "Mil"]
+interface PokemonListInterface{
+    name: string;
+    url: string;
+}
+
+
 
 export const Pokedex: React.FC<PokedexProps> = () => {
 
-    const [pokemons, setPokemons] = useState<string[]>(pokemonsArray); //faz a leitura dos dados || modifica os dados
-    const [selectedPokemon, setSelectedPokemon ] = useState<string | undefined>(undefined);
+    const [pokemons, setPokemons] = useState<PokemonListInterface[]>([]); //faz a leitura dos dados || modifica os dados
+    const [selectedPokemon, setSelectedPokemon ] = useState<PokemonListInterface | undefined>(undefined);
+    const [selectedPokemonDetails, setSelectedPokemonDetails ] = useState<any| undefined>(undefined);
+
+    useEffect(() => {
+       axios.get('https://pokeapi.co/api/v2/pokemon').then((response) => setPokemons(response.data.results)) 
+
+
+    }, []);
+
+    useEffect(() => {
+        if(!selectedPokemon) return;
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon.name}`).then((response) => setSelectedPokemonDetails(response.data)) 
+        
+        
+        
+    }, [selectedPokemon]);
 
     return (
         <div>
             <h1>pokedex</h1>
 
             pokemons:
-            {pokemons.map((pokemons) => <button onClick={() => setSelectedPokemon(pokemons)}>{pokemons}</button>)}
+            {pokemons.map((pokemon) => <button onClick={() => setSelectedPokemon(pokemon)}>{pokemon.name}</button>)}
 
-            <h2>Pokemon selecionado: {selectedPokemon || `Nenhum pokemon selecionado`}</h2>
+            <h2>Pokemon selecionado: {selectedPokemon?.name || `Nenhum pokemon selecionado`}</h2>
+            {JSON.stringify(selectedPokemonDetails, undefined, 2)}
         </div>
     );
 };
